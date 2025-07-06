15b9dfcb73d7acab87c7391ad4d596b145440497
Danny T <cobreti_@hotmail.com>
2025-07-06 17:12:40 -0400

    updated for SPA

--- webappclient/src/app/app.config.ts
+++ webappclient/src/app/app.config.ts
@@ -1,15 +1,38 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { MsalModule, MsalService, MsalGuard } from '@azure/msal-angular';
-import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { MsalModule, MsalService, MsalGuard, MsalInterceptorConfiguration, MsalGuardConfiguration, MSAL_INSTANCE, MSAL_GUARD_CONFIG, MSAL_INTERCEPTOR_CONFIG, MsalBroadcastService, MsalInterceptor } from '@azure/msal-angular';
+import { BrowserCacheLocation, InteractionType, IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
 import { msalConfig } from './auth-config';
 import { msalInterceptor } from './msal-interceptor';
 
-console.log('value1: ', msalConfig.auth.clientId);
-console.log('value2: ', msalConfig.auth.authority);
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication(msalConfig);
+}
+
+// MSAL Interceptor is required to request access tokens in order to access the protected resource (Graph)
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, Array<string>>();
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap
+  };
+}
+
+// MSAL Guard is required to protect routes and require authentication before accessing protected routes
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read']
+    }
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -36,6 +59,24 @@ export const appConfig: ApplicationConfig = {
       )
     ),
     MsalService,
-    MsalGuard
+    MsalGuard,
+    MsalBroadcastService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory
+    },
   ]
 };
--- webappclient/src/app/app.ts
+++ webappclient/src/app/app.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Inject, OnInit, inject } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
+import { MSAL_GUARD_CONFIG, MsalBroadcastService, MsalGuardConfiguration, MsalRedirectComponent, MsalService } from '@azure/msal-angular';
 import { AuthService } from './auth.service';
 import { LoginComponent } from './components/login/login.component';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { EventMessage, InteractionStatus, EventType, RedirectRequest, AuthenticationResult, InteractionType, PopupRequest } from '@azure/msal-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,11 +14,128 @@ import { LoginComponent } from './components/login/login.component';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
+export class App extends MsalRedirectComponent {
   protected title = 'webappclient';
   isLoggedIn = false;
-  private authService = inject(AuthService);
+  loginDisplay = false;
+  tokenExpiration: string = '';
+  private readonly _destroying$ = new Subject<void>();
 
-  ngOnInit(): void {
+  constructor(
+    @Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration,
+    private appAuthService: MsalService,
+    private msalBroadcastService: MsalBroadcastService
+  ) {
+    super(appAuthService);
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+
+    this.appAuthService.instance.enableAccountStorageEvents();
+
+    this.msalBroadcastService.inProgress$
+        .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$)
+      )
+      .subscribe(() => {
+        this.setLoginDisplay();
+        this.checkAndSetActiveAccount();
+      });
+
+    this.msalBroadcastService.msalSubject$
+        .pipe(
+          filter(
+            (msg: EventMessage) => msg.eventType === EventType.LOGOUT_SUCCESS
+          ),
+          takeUntil(this._destroying$)
+        )
+        .subscribe((result: EventMessage) => {
+          this.setLoginDisplay();
+          this.checkAndSetActiveAccount();
+        });
+
+    this.msalBroadcastService.msalSubject$
+        .pipe(
+          filter(
+            (msg: EventMessage) => msg.eventType === EventType.LOGIN_SUCCESS
+          ),
+          takeUntil(this._destroying$)
+        )
+        .subscribe((result: EventMessage) => {
+          const payload = result.payload as AuthenticationResult;
+          this.appAuthService.instance.setActiveAccount(payload.account);
+        });
+
+      // Used for storing and displaying token expiration
+    this.msalBroadcastService.msalSubject$.pipe(filter((msg: EventMessage) => msg.eventType === EventType.ACQUIRE_TOKEN_SUCCESS)).subscribe(msg => {
+      this.tokenExpiration=  (msg.payload as any).expiresOn;
+      localStorage.setItem('tokenExpiration', this.tokenExpiration);
+    });
+
+
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.appAuthService.instance.getAllAccounts().length > 0;
+  }
+
+  checkAndSetActiveAccount() {
+      /**
+       * If no active account set but there are accounts signed in, sets first account to active account
+       * To use active account set here, subscribe to inProgress$ first in your component
+       * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
+       */
+      let activeAccount = this.appAuthService.instance.getActiveAccount();
+
+      if (!activeAccount && this.appAuthService.instance.getAllAccounts().length > 0) {
+        let accounts = this.appAuthService.instance.getAllAccounts();
+        // add your code for handling multiple accounts here
+        this.appAuthService.instance.setActiveAccount(accounts[0]);
+      }
+  }
+
+  login() {
+    if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+        if (this.msalGuardConfig.authRequest) {
+          this.appAuthService.loginPopup({
+            ...this.msalGuardConfig.authRequest,
+          } as PopupRequest)
+            .subscribe((response: AuthenticationResult) => {
+              this.appAuthService.instance.setActiveAccount(response.account);
+            });
+        } else {
+          this.appAuthService.loginPopup()
+            .subscribe((response: AuthenticationResult) => {
+              this.appAuthService.instance.setActiveAccount(response.account);
+            });
+        }
+    } else {
+        if (this.msalGuardConfig.authRequest) {
+          this.appAuthService.loginRedirect({
+            ...this.msalGuardConfig.authRequest,
+          } as RedirectRequest);
+        } else {
+          this.appAuthService.loginRedirect();
+        }
+    }
+  }
+
+  logout() {
+if (this.msalGuardConfig.interactionType === InteractionType.Popup) {
+        this.appAuthService.logoutPopup({
+          account: this.appAuthService.instance.getActiveAccount(),
+        });
+      } else {
+        this.appAuthService.logoutRedirect({
+          account: this.appAuthService.instance.getActiveAccount(),
+        });
+      }
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined);
+    this._destroying$.complete();
   }
 }
